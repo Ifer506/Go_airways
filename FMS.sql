80637154f5e8ad69f8e8b1c12ef8782177f2a4bc
Ruchan10 <84487625+Ruchan10@users.noreply.github.com>
2022-02-12 10:32:34 +0545

    More database table added

--- FMS.sql
+++ FMS.sql
@@ -22,11 +22,51 @@ create table TravellersDetails(
 fullName varchar(40),
 nationality varchar(20));
 
+create table updateDetails(
+flightNumber varchar(20),
+Dep_Time varchar(20),
+Arr_Time varchar(20),
+Detaination varchar(20) 
+);
+
+create table register_gui(
+fName varchar(20),
+lName varchar(20),
+uName varchar(30),
+eMail varchar(50),
+pNumber varchar(15),
+password varchar(30),
+cPassword varchar(30),
+gender varchar(7),
+date varchar(20));
+
+create table AddUser(
+fName varchar(20),
+lName varchar(20),
+uName varchar(20),
+eMail varchar(40),
+pNumber varchar(14)
+);
+
+create table UpdateFlight(
+fNumber varchar(10),
+dTime varchar(20),
+aTime varchar(20),
+destination varchar(20),
+flightCost varchar(10)
+);
+
+select * from register_gui;
 select * from searchDetails;
 select * from travellers;
 select * from ContactDetails;
 select * from TravellersDetails;
+select * from updateDetails;
+select * from adduser;
+select * from UpdateFlight;
 
+drop table register_gui;
+drop table AddUser;
 drop table customer;
 drop table travellers;
 drop table TravellersDetails;
